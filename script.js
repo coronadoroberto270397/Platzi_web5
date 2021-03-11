3f7fc2f9cd8fd499116df3b60e4cfd95311fb57a
RobertoCoronado <robertocoronadocoto@gmail.com>
2021-03-11 11:17:26 -0600

    Se agrego funcion de entrada de datos

--- script.js
+++ script.js
@@ -3,21 +3,9 @@ var boton = document.getElementById("botoncito");
 boton.addEventListener("click",dibujoPorClick);
 
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas =30;
-var l = 0;
-var yi, xf;
-var colorcito = "RED";
 
-for(l = 0; l < lineas; l++)
-{
-    yi= 10 * l;
-    xf= 10 * (l + 1);
-    dibujarlinea(colorcito,0,yi,xf,300);
-    console.log("linea" + 1);
-}
-dibujarlinea(colorcito,1,1,1,299);
-dibujarlinea(colorcito,1,299,299,299);
 function dibujarlinea(color, xinicial, yinicial, xfinal,yfinal)
     {
         lienzo.beginPath();
@@ -27,8 +15,22 @@ function dibujarlinea(color, xinicial, yinicial, xfinal,yfinal)
         lienzo.stroke();
         lienzo.closePath();
     }
+
 function dibujoPorClick()
     {
-        alert("Hola");
-        var xxx= parseint(texto.value);
+    var lineas = parseInt(texto.value);
+    var l = 0;
+    var yi, xf;
+    var colorcito = "RED";
+    var espacio = ancho/ lineas;
+
+    for(l = 0; l < lineas; l++)
+    {
+    yi= espacio * l;
+    xf= espacio * (l + 1);
+    dibujarlinea(colorcito,0,yi,xf,300);
+    console.log("linea" + 1);
+    }
+    dibujarlinea(colorcito,1,1,1,299);
+    dibujarlinea(colorcito,1,299,299,299);
     }
